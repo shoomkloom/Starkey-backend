d2ec583ada67fb487b33965886a8d3937b6c2450
shoomkloom <shoomkloom@gmail.com>
2025-07-18 12:45:35 +0300

    Hardcode mongo connection string for exercise

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config({ quiet: true });
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const client = new MongoClient('mongodb+srv://starkey:Kloom1234@cluster0.fcsm7.mongodb.net/'); //Hardcoded for simplicity, use environment variables in production
 let db;
 
 async function connectToMongo() {
     if (!db) {
         await client.connect();
-        db = client.db(process.env.MONGODB_DBNAME);
+        db = client.db('starkey'); //Hardcoded for simplicity, use environment variables in production
     }
     return db;
 }
